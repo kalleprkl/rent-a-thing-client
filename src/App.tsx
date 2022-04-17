022a99735ff34a1de9fb174d3f413681a535215c
kp <kalle.perkoila@gmail.com>
2022-04-18 00:36:45 +0300

    trying downgrading react

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,69 @@
+import Accordion from "@mui/material/Accordion"
+import AccordionSummary from "@mui/material/AccordionSummary"
+import AccordionDetails from "@mui/material/AccordionDetails"
+import Typography from "@mui/material/Typography"
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
+import Box from '@mui/material/Box';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import { FixedSizeList, ListChildComponentProps } from 'react-window';
+
 import React from "react"
-import { useFetchAllThingsQuery } from "./app/api"
+import { useFetchAllThingsQuery, useFetchContractsByCustomerQuery } from "./app/api"
 
 const App = () => {
   const { data: things } = useFetchAllThingsQuery()
   return (
     <div>
-      {things?.map(thing => thing.id)}
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Accordion 1</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Box
+            sx={{ width: '100%', height: 400, maxWidth: 360, bgcolor: 'background.paper' }}
+          >
+            <FixedSizeList
+              height={400}
+              width={360}
+              itemSize={46}
+              itemCount={200}
+              overscanCount={5}
+            >
+              {renderRow}
+            </FixedSizeList>
+          </Box>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography>Accordion 2</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion disabled>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3a-content"
+          id="panel3a-header"
+        >
+          <Typography>Disabled Accordion</Typography>
+        </AccordionSummary>
+      </Accordion>
     </div>
   )
 }
--- src/app/api.ts
+++ src/app/api.ts
@@ -4,13 +4,19 @@ export const rentalApi = createApi({
     reducerPath: "rentalApi",
     baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api"}),
     endpoints: builder => ({
+        fetchAllCustomers: builder.query<Customer[], void>({
+            query: () => "Customers"
+        }),
+        fetchContractsByCustomer: builder.query<Contract[], void>({
+            query: customerId => `Customers${customerId}`
+        }),
         fetchAllThings: builder.query<Thing[], void>({
           query: () => "Things",
         }),
       }),
 })
 
-export const { useFetchAllThingsQuery } = rentalApi
+export const { useFetchAllThingsQuery, useFetchContractsByCustomerQuery } = rentalApi
 
 export interface Customer {
     id: number
