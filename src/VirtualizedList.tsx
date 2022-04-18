ea736fadfe3fea7edb169bd100ce83fdb4e61846
kp <kalle.perkoila@gmail.com>
2022-04-18 23:53:21 +0300

    refactoring

--- src/VirtualizedList.tsx
+++ src/VirtualizedList.tsx
@@ -2,34 +2,34 @@ import Box from '@mui/material/Box';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
+import { FixedSizeList, ListChildComponentProps } from 'react-window';
 
 import * as React from 'react';
-import { FixedSizeList, ListChildComponentProps } from 'react-window';
-import { AppData } from "./app/api"
-import { useFetchAllThingsQuery, UseQueryApi } from "./app/api"
+import { AppData, UseQueryApi } from "./app/api"
 
 interface VirtualizedListProps {
   api: UseQueryApi
 }
 
-export default function VirtualizedList(props: VirtualizedListProps) {
-  const { data } = props.api()
-  const renderRow = (props: ListChildComponentProps<Array<AppData>>) => {
-    const { index, style } = props
-    if (!data) {
-      return <p/>
-    }
-    return (
-      <ListItem style={style} key={index} component="div" disablePadding>
-        <ListItemButton>
-          <ListItemText primary={`Item ${data[index].id}`} />
-        </ListItemButton>
-      </ListItem>
-    )
+const renderRow = (data: Array<AppData> | undefined) => (props: ListChildComponentProps<Array<AppData>>) => {
+  const { index, style } = props
+  if (!data) {
+    return <p/>
   }
   return (
+    <ListItem style={style} key={index} component="div" disablePadding>
+      <ListItemButton>
+        <ListItemText primary={`Item ${data[index].id}`} />
+      </ListItemButton>
+    </ListItem>
+  )
+}
+
+export default function VirtualizedList(props: VirtualizedListProps) {
+  const { data } = props.api()
+  return (
     <Box
-      sx={{ width: '100%', height: 400, maxWidth: 360, bgcolor: 'background.paper' }}
+      sx={{ width: '100%', height: 200, maxWidth: 360, bgcolor: 'background.paper' }}
     >
       <FixedSizeList
         height={400}
@@ -38,7 +38,7 @@ export default function VirtualizedList(props: VirtualizedListProps) {
         itemCount={data?.length ?? 0}
         overscanCount={5}
       >
-        {renderRow}
+        {renderRow(data)}
       </FixedSizeList>
     </Box>
   );
