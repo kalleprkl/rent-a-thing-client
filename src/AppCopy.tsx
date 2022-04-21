34d3e8428bdcb88ddaf9d09a761f6ae70884a1fb
kp <kalle.perkoila@gmail.com>
2022-04-21 23:07:07 +0300

    looking better

--- src/AppCopy.tsx
+++ src/AppCopy.tsx
@@ -5,47 +5,15 @@ import Typography from "@mui/material/Typography"
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
 import VirtualizedList from "./VirtualizedListCopy"
 
-import { Route, Routes, useLocation, useNavigate, useParams } from "react-router-dom"
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom"
 
 import React, { useEffect } from "react"
-import { skipToken } from "@reduxjs/toolkit/dist/query"
-import { useFetchAllCustomersQuery, AppData, QueryAction, useFetchContractsByCustomerQuery } from "./app/api"
-
-interface AppAccordionProps {
-  //name: string,
-  //next: string,
-  //query: QueryAction
-}
-
-const AppAccordion: React.FC<AppAccordionProps> = props => {
-  return (
-    <Accordion onChange={(event, expanded) => ""} >
-      <AccordionSummary
-        expandIcon={<ExpandMoreIcon />}
-        aria-controls="panel1a-content"
-        id="panel1a-header"
-      >
-        <Typography>{""}</Typography>
-      </AccordionSummary>
-      <AccordionDetails>
-        {
-          <Routes>
-            <Route path={""} element={<VirtualizedList />} />
-          </Routes>
-        }
-      </AccordionDetails>
-    </Accordion>
-  )
-}
+import { SkipToken, skipToken } from "@reduxjs/toolkit/dist/query"
+import { useFetchAllCustomersQuery, useFetchContractsByCustomerQuery } from "./app/api"
 
 interface PathObject {
   name: string,
   id: number
-}
-
-interface QueryActions {
-  Customers: typeof useFetchAllCustomersQuery,
-  Contracts: typeof useFetchContractsByCustomerQuery
 }
 
 export const App = () => {
@@ -56,19 +24,35 @@ export const App = () => {
   const pathObjects = []
   let pathObject = {} as PathObject
   for (const section of pathSections) {
+    const { id, name } = pathObject
     const maybeId = Number(section)
+    // This assumes path form to be name/id/name/id
     if (maybeId) {
       pathObject.id = maybeId
-      pathObjects.push(pathObject)
-      pathObject = {} as PathObject
     } else {
-      pathObject.name = section
+      if (!name) {
+        pathObject.name = section
+        pathObjects.push(pathObject)
+        pathObject = {} as PathObject
+      } else {
+        pathObjects.push(pathObject)
+        pathObject = { name } as PathObject
+      }
     }
   }
-  const appElements = pathObjects.map(({ name, id }) => {
-    let data: Array<AppData> | undefined
-    data = useFetchAllCustomersQuery(name === "Customers" ? undefined : skipToken).data
-    data = useFetchContractsByCustomerQuery(name === "Contracts" ? id ?? skipToken : skipToken).data
+  console.log(pathObjects)
+  let customers: undefined | SkipToken, contracts: number | SkipToken = skipToken
+  for (const { name, id } of pathObjects) {
+    if (name === "Customers") {
+      customers = undefined
+    }
+    if (name == "Contracts" && id) {
+      contracts = id
+    }
+  }
+  const customersData = useFetchAllCustomersQuery(customers).data
+  const contractsData = useFetchContractsByCustomerQuery(contracts).data
+  const appElements: Array<React.FC> = [customersData, contractsData].map(data => {
     return () => (
       <Accordion onChange={(event, expanded) => ""} >
         <AccordionSummary
@@ -87,12 +71,12 @@ export const App = () => {
         </AccordionDetails>
       </Accordion>
     )
-  })
-  return (
-    <div style={{ width: "60%", marginLeft: "auto", marginRight: "auto" }}>
-      {appElements.map(AppElement => <AppElement />)}
-    </div>
-  )
+})
+return (
+  <div style={{ width: "60%", marginLeft: "auto", marginRight: "auto" }}>
+    {appElements.map(AppElement => <AppElement />)}
+  </div>
+)
 }
 
 export default App;
